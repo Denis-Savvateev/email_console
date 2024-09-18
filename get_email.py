ed0d9d492d623ee6128396055f8d9e8a3fd7fbf5
Денис Савватеев <densavvateev@yandex.ru>
2024-09-18 23:14:43 +0300

    Make main functionality of this miniproject.

---
+++ get_email.py
@@ -0,0 +1,49 @@
+import datetime as dt
+import email
+from email.header import decode_header
+import imaplib
+from itertools import islice
+# import base64
+# from bs4 import BeautifulSoup
+# import re
+
+from config import EMAIL_DATA, NUMBER_OF_LETTERS
+
+
+def get_mail(server, username, password, number_of_letters):
+    imap = imaplib.IMAP4_SSL(server)
+    imap.login(username, password)
+    imap.select("INBOX")
+    print(f'Последние {number_of_letters} писем учетной записи: {username}')
+    unseen_msgs = imap.uid('search', 'UNSEEN', 'ALL')[1][0].split()
+    all_msg = (imap.uid('search', 'ALL')[1][0].split())
+    for idx in islice(reversed(all_msg), number_of_letters):
+        _, raw_msg = imap.uid('fetch', idx, '(RFC822)')
+        msg = email.message_from_bytes(raw_msg[0][1])
+        letter_date = dt.datetime(*email.utils.parsedate_tz(msg["Date"])[:5])
+        if type(decode_header(msg['From'])[0][0]) is bytes:
+            letter_from = decode_header(msg['From'])[0][0].decode()
+        else:
+            letter_from = msg['From']
+        letter_ret_path = msg['Return-path']
+        header = decode_header(msg["Subject"])[0][0].decode()
+        print(
+            f'Время письма: {letter_date.strftime("%d.%m.%Y %H:%M:%S")}, '
+            f'Получено от {letter_from}, Обратный адрес: {letter_ret_path}, '
+            f'Тема: {header}'
+        )
+        if idx in unseen_msgs:
+            imap.uid('STORE', idx, '-FLAGS', '(\Seen)')  # W605
+
+
+def main():
+    number_of_letters: int = NUMBER_OF_LETTERS
+    for box in EMAIL_DATA:
+        server = box['server']
+        username = box['username']
+        password = box['password']
+        get_mail(server, username, password, number_of_letters)
+
+
+if __name__ == '__main__':
+    main()
