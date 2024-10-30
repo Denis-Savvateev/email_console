07892f15669218a567e8db6d9d9a43bdd5b18295
Денис Савватеев <densavvateev@yandex.ru>
2024-10-31 02:59:07 +0300

    Add output text of message. Add table output.

--- get_email.py
+++ get_email.py
@@ -1,15 +1,30 @@
+import base64
 import datetime as dt
 import email
 from email import policy
 from email.header import decode_header
 from email.message import EmailMessage
+import html2text
 import imaplib
 from itertools import islice
-# import base64
-# from bs4 import BeautifulSoup
-# import re
+from prettytable import PrettyTable, HRuleStyle
 
 from config import EMAIL_DATA, NUMBER_OF_LETTERS
+
+TABLE_MAX_WIGHT = 160
+"""Максимальная ширина таблицы."""
+FIELD_MIN_WIGHT = 15
+"""Минимальная ширина поля таблицы."""
+FIELD_MAX_WIGHT = 100
+"""Максимальная ширина поля таблицы."""
+INCLUDE_TEXT = True
+"""Включать текст в таблицу"""
+
+TEXT_MAKER = html2text.HTML2Text()
+TEXT_MAKER.ignore_links = True
+TEXT_MAKER.ignore_tables = True
+TEXT_MAKER.body_width = 0
+TEXT_MAKER.single_line_break = True
 
 
 def get_mail(server, username, password, number_of_letters):
@@ -19,36 +34,67 @@ def get_mail(server, username, password, number_of_letters):
     print(f'Последние {number_of_letters} писем учетной записи: {username}')
     unseen_msgs = imap.uid('search', 'UNSEEN', 'ALL')[1][0].split()
     all_msg = (imap.uid('search', 'ALL')[1][0].split())
+    table = PrettyTable()
+    field_names = [
+        'Время письма', 'Получено от', 'Обратный адрес', 'Тема'
+    ]
+    if INCLUDE_TEXT:
+        field_names.append('Текст')
+    table.field_names = field_names
+    table.max_table_width = TABLE_MAX_WIGHT
+    table.min_width = FIELD_MIN_WIGHT
+    table.max_width = FIELD_MAX_WIGHT
+    table.hrules = HRuleStyle.ALL
     for idx in islice(reversed(all_msg), number_of_letters):
         _, raw_msg = imap.uid('fetch', idx, '(RFC822)')
-        msg: EmailMessage = email.message_from_bytes(raw_msg[0][1], policy=policy.default)
+        msg: EmailMessage = email.message_from_bytes(
+            raw_msg[0][1], policy=policy.default
+        )
         letter_date = dt.datetime(*email.utils.parsedate_tz(msg["Date"])[:5])
         if type(decode_header(msg['From'])[0][0]) is bytes:
             letter_from = decode_header(msg['From'])[0][0].decode()
         else:
             letter_from = msg['From']
-        content = msg.get_body().get_content()
+        for part in msg.walk():
+            if part.get_content_type() == 'text/html':
+                text = TEXT_MAKER.handle(part.get_content())
+            elif part.get_content_type() == 'text/plain':
+                text = part.get_content()
+        text = text.replace('\n', '  ').replace('\r', '  ')
         letter_ret_path = msg['Return-path']
         header = decode_header(msg["Subject"])[0][0]
-        print(
-            f'Время письма: {letter_date.strftime("%d.%m.%Y %H:%M:%S")}, '
-            f'Получено от {letter_from}, Обратный адрес: {letter_ret_path}, '
-            f'Тема: {header}'
-        )
+        row = [
+            letter_date.strftime("%d.%m.%Y %H:%M:%S"),
+            letter_from,
+            letter_ret_path,
+            header,
+        ]
+        if INCLUDE_TEXT:
+            row.append(text)
+        table.add_row(row)
         if idx in unseen_msgs:
             imap.uid('STORE', idx, '-FLAGS', '(\Seen)')  # W605
         # read_mesage = input('Вывести текст письма? 1 - да')
         # if read_mesage == '1':
         #     payload = msg.get_payload()
+    print(table)
+    print('')
 
 
 def main():
-    number_of_letters: int = NUMBER_OF_LETTERS
-    for box in EMAIL_DATA:
-        server = box['server']
-        username = box['username']
-        password = box['password']
-        get_mail(server, username, password, number_of_letters)
+    print('')
+    choice = ''
+    while choice != '0':
+        number_of_letters: int = NUMBER_OF_LETTERS
+        for box in EMAIL_DATA:
+            server = box['server']
+            username = box['username']
+            password = box['password']
+            get_mail(server, username, password, number_of_letters)
+        choice = input(
+            'Для повторной проверки нажмите Enter. Для выхода из программы - '
+            '0 и Enter:'
+            )
 
 
 if __name__ == '__main__':
